c46ae60afd9c51924947d7ffee31e2d9efb13481
sachith-1 <cst17043@std.uwu.ac.lk>
2020-08-02 20:42:43 +0530

    snd

--- Car.php
+++ Car.php
@@ -1,13 +1,11 @@
 <?php
 
 class Car{
-
     private $modal="N/A";
 
     public function __construct($modal="Test"){
         $this->modal=$modal;
         echo $this->modal;
     }
-
 }
 $car=new Car();
